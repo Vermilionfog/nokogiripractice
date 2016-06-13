2687ec9d0e8eccc6abc67b6b1d20b8c89259310c
Vermilionfog <amagatsu@outlook.jp>
2016-06-13 16:19:12 +0000

    Push and pull test

--- app/controllers/scraping_controller.rb
+++ app/controllers/scraping_controller.rb
@@ -174,6 +174,7 @@ class ScrapingController < ApplicationController
     html_change_detecter.set_reloding_timer(10)
     html_change_detecter.enable_timer
     
+    puts "Push and Pull Test"
     render 'scraping/testing_page'
   end
   
